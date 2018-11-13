a17d0b0bd16a4f78f95b0365a2d31045cbd92c73
Quyen Nguyen <ntquyen98@gmail.com>
2018-11-13 19:43:36 +0700

    some page

--- ABC/ABC/MainWindow.xaml.cs
+++ ABC/ABC/MainWindow.xaml.cs
@@ -23,8 +23,8 @@ namespace ABC
         public MainWindow()
         {
             InitializeComponent();
+            frMainMaxnimumsize.Content = new HomePageMaximumsize();
         }
-
         private void CloseProgram_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
@@ -34,17 +34,29 @@ namespace ABC
         {
             this.WindowState = WindowState.Minimized;
         }
-
         private void btnOpenMenu_Click(object sender, RoutedEventArgs e)
         {
             btnOpenMenu.Visibility = Visibility.Collapsed;
             btnCloseMenu.Visibility = Visibility.Visible;
+            frMainMaxnimumsize.Content = null;
+            frMainMinisize.Content = new HomePageMinisize();
         }
 
         private void btnCloseMenu_Click(object sender, RoutedEventArgs e)
         {
             btnOpenMenu.Visibility = Visibility.Visible;
             btnCloseMenu.Visibility = Visibility.Collapsed;
+            frMainMinisize.Content = null;
+            frMainMaxnimumsize.Content = new HomePageMaximumsize();
+        }
+        private void frMainMinisize_ContentRendered(object sender, EventArgs e)
+        {
+            frMainMinisize.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
+        }
+
+        private void frMainMaxnimumsize_ContentRendered(object sender, EventArgs e)
+        {
+            frMainMaxnimumsize.NavigationUIVisibility = System.Windows.Navigation.NavigationUIVisibility.Hidden;
         }
     }
 }
