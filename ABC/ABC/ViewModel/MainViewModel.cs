288c778409236e37a3e249e7dcb53189afc4d267
Le Quoc Duy Quang <duyquangbtx@gmail.com>
2019-01-02 01:49:58 +0700

    custom viewmodel and resourceXAML

---
+++ ABC/ABC/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ABC.ViewModel
+{
+	public class MainViewModel : BaseViewModel
+	{
+		public bool isShow = false;
+		public ICommand LoadedWindowCommand { get; set; }
+		public MainViewModel()
+		{
+			LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+			{
+				isShow = true;
+				Login loginWindow = new Login();
+				loginWindow.ShowDialog();
+			});
+		}
+	}
+}
