2c36728cb58cef924c3b987c4c13457309480d03
QuangNguyen <ntquang17021998@gmail.com>
2019-01-03 09:55:37 +0700

    hoan thanh ui logic

--- ABC/ABC/ViewModel/DoiMatKhauViewModel.cs
+++ ABC/ABC/ViewModel/DoiMatKhauViewModel.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ABC.Commands;
+using MaterialDesignThemes.Wpf;
 
 namespace ABC.ViewModel
 {
     public class DoiMatKhauViewModel:BindableBase
     {
+        public DoiMatKhauViewModel()
+        {
+            SaveCommand = new MyICommand(OnSave);
+            CancelCommand = new MyICommand(OnCancel);
+        }
 
+        #region Command
+        public MyICommand SaveCommand { get; private set; }
+        public MyICommand CancelCommand { get; private set; }
+
+        void OnSave()
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
+        }
+
+        void OnCancel()
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
+        }
+        #endregion
     }
 }
--- ABC/ABC/ViewModel/ThayDoiTenDangNhapViewModel.cs
+++ ABC/ABC/ViewModel/ThayDoiTenDangNhapViewModel.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ABC.Commands;
+using MaterialDesignThemes.Wpf;
 
 namespace ABC.ViewModel
 {
-    class ThayDoiTenDangNhapViewModel
+    public class ThayDoiTenDangNhapViewModel:BindableBase
     {
+        public ThayDoiTenDangNhapViewModel()
+        {
+            SaveCommand = new MyICommand(OnSave);
+            CancelCommand = new MyICommand(OnCancel);
+        }
+
+        #region Command
+        public MyICommand SaveCommand { get; private set; }
+        public MyICommand CancelCommand { get; private set; }
+
+        void OnSave()
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
+        }
+
+        void OnCancel()
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
+        }
+        #endregion
+
     }
 }
