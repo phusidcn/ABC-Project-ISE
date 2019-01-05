a48601ec6a35941c99c742fb33066a1773f23c2c
Le Quoc Duy Quang <duyquangbtx@gmail.com>
2019-01-05 15:18:14 +0700

    custom themthu themno vm

--- ABC/ABC/ViewModel/ThemThuViewModel.cs
+++ ABC/ABC/ViewModel/ThemThuViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ABC.Helper;
+using ABC.Model;
 using MaterialDesignThemes.Wpf;
 
 namespace ABC.ViewModel
@@ -15,7 +16,52 @@ namespace ABC.ViewModel
             okCommand = new MyICommand(onOk);
             cancelCommand = new MyICommand(onCancel);
         }
-        public MyICommand okCommand { get; private set; }
+
+		#region Properties
+
+		private List<Vi> _viList;
+		public List<Vi> ViList
+		{
+			get { return _viList; }
+			set { SetProperty(ref _viList, value); }
+		}
+
+		private DateTime _ngay;
+		public DateTime Ngay
+		{
+			get { return _ngay; }
+			set { SetProperty(ref _ngay, value); }
+		}
+
+		private string _loai;
+		public string Loai
+		{
+			get { return _loai; }
+			set { SetProperty(ref _loai, value); }
+		}
+		private int _idgiaodich;
+		public int IDGiaoDich
+		{
+			get { return _idgiaodich; }
+			set { SetProperty(ref _idgiaodich, value); }
+		}
+
+		private string _person;
+		public string Person
+		{
+			get { return _person; }
+			set { SetProperty(ref _person, value); }
+		}
+
+		private int _stt;
+		public int STT
+		{
+			get { return _stt; }
+			set { SetProperty(ref _stt, value); }
+		}
+		#endregion
+
+		public MyICommand okCommand { get; private set; }
         public MyICommand cancelCommand { get; private set; }
 
         void onOk()
